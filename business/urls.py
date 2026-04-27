2ad168c2205d23bbfc7bfe1905a0bf075fc6a77c
Lucasvieirar <lucasvieiraribeiro2055@gmail.com>
2026-04-27 19:22:48 +0000

    criar o serialIzers da API

--- business/urls.py
+++ business/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = {
-    path('business/', ..., name='business-create-list')
+    path('business/', views.BusinessCreateListView.as_view(), name='business-create-list')
 }
